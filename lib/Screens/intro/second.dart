de8f21a6ffd0dab6a5c9bc11cd11349b9418387e
jvlguanco <jvlguanco@gmail.com>
2022-11-03 02:22:53 +0800

    New Splash Screen

---
+++ lib/Screens/intro/second.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color(0xFF2F83D6),
+        body: Container(
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    height: MediaQuery.of(context).size.height/1.58,
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 30.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Image.asset(
+                            'assets/Logo_2.png',
+                            height: 50,
+                          ),
+                          Image.asset(
+                            'assets/people/first.png',
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Stack(
+                children: [
+                  Container(
+                    height: MediaQuery.of(context).size.height/2.72414,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(50.0),
+                            topLeft: Radius.circular(50.0)
+                        )
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      SizedBox(height: 20.0,),
+                      Center(
+                        child: Text(
+                          'Easy to spot a yellow car when you are \nalways thinking of a yellow car.',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontFamily: 'Inter',
+                              fontSize: 13.0,
+                              color: Color(0xFF6E6E6E)
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 15.0,),
+                      Center(
+                        child: Text(
+                          'Easy to spot opportunity when you \nare always thinking of opportunity.',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontFamily: 'Inter',
+                              fontSize: 16.0,
+                              color: Color(0xFF2F83D6),
+                              fontWeight: FontWeight.w700
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 25.0,),
+                      InkWell(
+                          onTap: (){
+
+                          },
+                          splashColor: Colors.white10,
+                          child: Image.asset(
+                            'assets/ring/ring_1.png',
+                            height: 100,
+                            width: 100,
+                          )
+                      )
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
+        )
+    );
+  }
+}
--- lib/Screens/splash/splash.dart
+++ lib/Screens/splash/splash.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:untitled/Screens/intro/first.dart';
+import '../intro/first.dart';
 import 'homepage.dart';
 
 class Root extends StatelessWidget {
@@ -16,65 +18,123 @@ class Root extends StatelessWidget {
 class Welcome extends StatelessWidget {
   const Welcome({Key? key}) : super(key: key);
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.teal[600],
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 280.0, 30.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Center(
-              child: Text(
-                'NAME',
-                style: TextStyle(
-                  fontSize: 48.0,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 2.0,
-                ),
-              ),
+      backgroundColor: Color(0xFF2F83D6),
+      body: Stack(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height/1.6,
+            decoration: BoxDecoration(
+            color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    bottomRight: Radius.circular(500.0),
+                    bottomLeft: Radius.circular(500.0)
+                )
             ),
-            SizedBox(height: 15.0),
-            Center(
-              child: Text(
-                'motto here',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white,
-                  letterSpacing: 2.0,
-                ),
-              ),
-            ),
-            SizedBox(height: 40.0),
-            Center(
-              child: TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => StartingScreen()
-                      ),
-                    );
-                  },
-                  style: TextButton.styleFrom(
-                    backgroundColor: Colors.teal[900],
-                  ),
-                  child: Padding(
-                    padding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-                    child: Text(
-                      'Go To Finance Manager',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16.0,
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 0.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  children: [
+                    Center(
+                      child: Text(
+                        'Apportunity',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 40.0,
+                            fontFamily: 'Inter',
+                            color: Color(0xFFFBBB00)
+                        ),
                       ),
                     ),
-                  )
-              ),
+                    SizedBox(height: 25.0),
+                    Center(
+                      child: Text(
+                        'Locate financial services \n& growth opportunities easier',
+                        style: TextStyle(
+                            fontSize: 16.0,
+                            fontFamily: 'Inter',
+                            color: Color(0xFF6E6E6E)
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ],
+                ),
+                Center(
+                    child: Image.asset('assets/Logo.png')
+                ),
+                SizedBox(height: 10.0),
+                Column(
+                  children: [
+                    Center(
+                      child: TextButton(
+                        onPressed: (){
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => FirstScreen()
+                              )
+                          );
+                        },
+                        style: TextButton.styleFrom(
+                            backgroundColor: Colors.white
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(50.0,5.0,50.0,5.0),
+                          child: Text(
+                            'Get Started',
+                            style: TextStyle(
+                                fontSize: 14.0,
+                                fontWeight: FontWeight.w600,
+                                fontFamily: 'Inter',
+                                color: Color(0xFF6E6E6E)
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 5.0),
+                    Center(
+                      child: TextButton(
+                        onPressed: (){
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => StartingScreen()
+                              )
+                          );
+                        },
+                        style: TextButton.styleFrom(
+                            backgroundColor: Colors.white
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(12.0,5.0,12.0,5.0),
+                          child: Text(
+                            'Go to Finance Manager',
+                            style: TextStyle(
+                                fontSize: 14.0,
+                                fontFamily: 'Inter',
+                                fontWeight: FontWeight.w600,
+                                color: Color(0xFF6E6E6E)
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(height: 10.0),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
