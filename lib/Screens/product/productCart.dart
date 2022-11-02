9cd474a77885df5efb82a18bf9f538b82ca3ae19
jvlguanco <jvlguanco@gmail.com>
2022-11-02 15:44:45 +0800

    Added Cart Total (No Cart Screen yet)

---
+++ lib/Screens/product/productCart.dart
@@ -0,0 +1,9 @@
+class AddToCart{
+  String name = '';
+  String image = '';
+  double price = 0.0;
+
+  static List<String> items = [];
+  static List<String> images = [];
+  static List<double> prices = [];
+}
